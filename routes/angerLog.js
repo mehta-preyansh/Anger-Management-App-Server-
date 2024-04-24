4f9d227417bd3e986768183ef3c4e034aedbee2f
mehta-preyansh <preyanshmehtaben@gmail.com>
2024-04-24 13:10:23 +0530

    Added delete route

--- routes/angerLog.js
+++ routes/angerLog.js
@@ -1,20 +1,21 @@
 const router = require('express').Router()
 const User = require("../modals/user")
 const bcrypt = require("bcrypt")
+const {ObjectId} = require('mongodb')
 
-router.post('/event', async (req, res)=>{
+router.post('/event', async (req, res) => {
   const username = req.body.username
   const data = req.body.details
-  const {reason, date, level} = {...data}
+  const { reason, date, level } = { ...data }
 
   // console.log(reason, level, date)
   const user = await User.findOne({ username });
 
-  if (!user || user==null) {
+  if (!user || user == null) {
     // User not found
     return res.status(401).send({ message: 'User does not exist' });
-  } 
-  else{
+  }
+  else {
     //Register log in the log array
     try {
       const newEvent = {
@@ -26,7 +27,7 @@ router.post('/event', async (req, res)=>{
       await user.save()
       // console.log("Success")
       return res.status(201).send({ message: 'Anger log registered successfully', status: 201 });
-      
+
     } catch (error) {
       // console.log("Error")
       return res.status(500).send({ message: 'Error registering event', status: 500 });
@@ -34,19 +35,31 @@ router.post('/event', async (req, res)=>{
   }
 })
 
-router.get('/event', async (req, res)=>{
+router.get('/events', async (req, res) => {
   const username = req.query.username
   const user = await User.findOne({ username });
-  if(user.events.length===0){
-    res.status(204).json({message: "No log events yet"})
+  if (user.events.length === 0) {
+    res.status(204).json({ message: "No log events yet" })
   }
-  else{
-    res.status(200).send({events: user.events.length > 50 ? dataArray.slice(0, 50) : user.events})
+  else {
+    res.status(200).send({ events: user.events.length > 50 ? dataArray.slice(0, 50) : user.events })
   }
 })
 
-// router.put('/event')
-
-// router.delete('/event')
+router.delete('/user/:userId/logs/:logId', async (req, res) => {
+  const userId = req.params.userId;
+  const logId = req.params.logId;
+  console.log(userId, logId)
+  try {
+    const result = await User.updateOne(
+      { _id: new ObjectId(userId) },
+      { $pull: { events: { _id: new ObjectId(logId) } } }
+    );
+    console.log(result)
+    res.status(200).send(`Resource with ID ${logId} deleted successfully.`);
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 module.exports = router
